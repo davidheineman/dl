ea9e0f5f0515d5aaba2d719250c173796cc3c8b0
David Heineman <dheineman3@gatech.edu>
2024-05-01 22:01:11 -0400

    delete extra files

--- data/get_dolma.sh
+++
@@ -1,8 +0,0 @@
-DATA_DIR="data"
-PARALLEL_DOWNLOADS=10
-DOLMA_VERSION="v1_6-sample"
-
-git clone https://huggingface.co/datasets/allenai/dolma
-mkdir -p "${DATA_DIR}"
-
-cat "dolma/urls/${DOLMA_VERSION}.txt" | xargs -n 1 -P "${PARALLEL_DOWNLOADS}" wget -q -P "$DATA_DIR"
